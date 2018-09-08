3762eef52bd01ee7d0aece8c1a84666ef8f8402f
yueshutong <923823264@qq.com>
2018-09-08 18:59:37 +0800

    解决不支持json为数组的bug

--- src/test/java/cn/zyzpp/AppTest.java
+++ src/test/java/cn/zyzpp/AppTest.java
@@ -5,12 +5,7 @@ import com.alibaba.fastjson.JSON;
 import net.sf.json.util.JSONTokener;
 import org.junit.Test;
 
-import javax.activation.MimetypesFileTypeMap;
-import java.io.File;
 import java.io.IOException;
-import java.net.FileNameMap;
-import java.net.URLConnection;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,28 +18,11 @@ public class AppTest {
 
     @Test
     public void test() {
-        String pathname = "D:\\IDEA\\workSpace\\JerryServer\\picture\\1532688969.png";//JDK优秀
-//        String pathname = "D:\\IDEA\\workSpace\\JerryServer\\webapps\\ROOT\\page.json";
-        String type = new MimetypesFileTypeMap().getContentType(new File(pathname));
-        System.out.println("第二种javax.activation: " + type);
 
-        try {
-            String s = Files.probeContentType(new File(pathname).toPath());
-            System.out.println("第三种java.nio: " + s);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        FileNameMap fileNameMap = URLConnection.getFileNameMap();
-        String contentType = fileNameMap.getContentTypeFor(pathname);
-        System.out.println("第四种java.net: " + contentType);
     }
 
     @Test
     public void test2() throws IOException{
-//        Connection.Response response = Jsoup.connect("https://www.kuaidi100.com/query?type=shentong&postid=3374107234608&id=1")
-//                .method(Connection.Method.GET)
-//                .execute();
         List<Object> list = new ArrayList<>();
         list.add(new EntityInter("123",1));
         list.add(new EntityInter("123",1));
